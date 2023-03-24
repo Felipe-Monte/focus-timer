8d64fe43b212cc26495aaa01f302ad9355cb136c
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2023-03-24 19:46:35 -0300

    update timer js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const buttonStop = document.querySelector('#stop')
 
 let minutesDisplay = document.querySelector('#minutes')
 let secondsDisplay = document.querySelector('#seconds')
+let userMinutes = "00"
 
 function resetTimerDisplay() {
   buttonPause.classList.add('hide')
@@ -13,44 +14,55 @@ function resetTimerDisplay() {
   buttonSet.classList.remove('hide')
 }
 
-function countdouwn() {
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
+
+function countdown() {
   setTimeout(function () {
-    let minutes = Number(minutesDisplay.textContent)
     let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
 
-    secondsDisplay.textContent = "00"
-
-    if (minutes <= 0) {
+    if (minutes == 0 && seconds == 0) {
+      updateTimerDisplay(userMinutes, 0)
       resetTimerDisplay()
       return
     }
 
     if (seconds <= 0) {
-      seconds = 2
+      seconds = 60
 
-      minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
+      // minutesDisplay.textContent = String(--minutes).padStart(2, "0")
+      updateTimerDisplay(--minutes, seconds)
     }
 
-    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
-
-
-    countdouwn()
-  }, 1000)
+    // secondsDisplay.textContent = String(--seconds).padStart(2, "0")
+    updateTimerDisplay(minutes, String(--seconds))
+    countdown()
+  }, 10)
 }
+
 
 buttonPlay.addEventListener('click', function () {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
   buttonSet.classList.add('hide')
   buttonStop.classList.remove('hide')
-  countdouwn()
+  countdown()
 })
 
 buttonPause.addEventListener('click', function () {
+  buttonPlay.classList.remove('hide')
+  buttonPause.classList.add('hide')
+})
+
+buttonStop.addEventListener('click', function () {
   resetTimerDisplay()
 })
 
 buttonSet.addEventListener('click', function () {
-  let minutes = prompt("Quantos minutos ?")
-  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  userMinutes = prompt("Quantos minutos ?")
+  // minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  updateTimerDisplay(String(userMinutes), 0)
 })
